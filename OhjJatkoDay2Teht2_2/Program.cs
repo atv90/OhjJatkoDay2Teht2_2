d5a2e92d689e491e490ecc85f8be5d3ea94395f6
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2017-12-07 10:30:05 +0200

    Ilmoittaa virheestä.

--- OhjJatkoDay2Teht2_2/Program.cs
+++ OhjJatkoDay2Teht2_2/Program.cs
@@ -3,13 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace OhjJatkoDay2Teht2_2
 {
-    class Program
+     class Program
     {
         static void Main(string[] args)
         {
+            //luodaan uusi XmlTextReader instanssi reader ja 
+            //lisätään siihen haluttu XML-tiesodto
+            string Books = @"C:\Users\Anne\source\repos\OhjJatkoDay2Teht2_2\Books.Xml";
+             XmlTextReader reader = new XmlTextReader(Books);
+
+            //luetaan XML-tiedosto läpi while-loopin ja reader-metodin avulla
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {   
+                    case XmlNodeType.Element:
+                        Console.Write("<" + reader.Name);
+                        while (reader.MoveToNextAttribute())
+                            Console.Write(" " + reader.Name + "='" + reader.Value + "'");
+                      
+                        Console.WriteLine(">");
+                        break;
+
+                    //esitetään joka elementissä teksti
+                    case XmlNodeType.Text:
+                        Console.WriteLine(reader.Value);
+                        break;
+
+                    //ilmoittaa elementin päättymisen
+                    case XmlNodeType.EndElement:
+                        Console.Write("</" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                    
+                }
+                Console.WriteLine(reader.Name);
+            }
+            Console.ReadLine();
         }
     }
 }
